15dc89b0e15e458fddfa586b52ffe032c7d839d8
zenito9970 <zenito9970@gmail.com>
2019-03-24 08:16:35 +0900

    refactored echo()

--- src/main.rs
+++ src/main.rs
@@ -77,7 +77,7 @@ fn draw(rb: &RustBox, remain: u64, table: &HashMap<char, ([&str; 6], usize)>) {
     rb.present();
 }
 
-fn echo(rb: &RustBox, symbol: &[&str; 6], start_x: usize, start_y: usize) {
+fn echo(rb: &RustBox, symbol: &[&str], start_x: usize, start_y: usize) {
     let mut y = start_y;
     for line in symbol {
         rb.print(start_x, y, rustbox::RB_NORMAL, Color::Default, Color::Default, line);
